7b123c46caf4c5b739ab68a7768f3f74191d30d4
dnssd <dnssdvlp@gmail.com>
2019-11-23 22:16:54 -0300

    Close menu - Mobile views

--- app/js/main.js
+++ app/js/main.js
@@ -1,17 +1,23 @@
 (function () {
 
     //Mobile button menu animation
+    var el = document.getElementById("buttonMobile");
+    var menu = document.getElementById("mobileMenu");
+
     document.getElementById("buttonMobile").onclick = function () {
-        mobileButtonTransition()
-    }
-
-    function mobileButtonTransition() {
-        var el = document.getElementById("buttonMobile");
         el.classList.toggle("c-button-mobile--active");
-
-        var menu = document.getElementById("mobileMenu");
         menu.classList.toggle("mobile-navigation__menu--active");
     }
+
+    //Close mobile menu
+    var items = document.querySelectorAll(".mobile-navigation__list li");
+
+    items.forEach(function (elem) {
+        elem.addEventListener('click', function () {
+            el.classList.remove("c-button-mobile--active");
+            menu.classList.remove("mobile-navigation__menu--active");
+        })
+    })
 
     //Smooth Scroll
     smoothie();
