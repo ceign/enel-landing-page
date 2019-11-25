907d1485641147c66f9dc72b9ce9d9467e51d98d
dnssd <dnssdvlp@gmail.com>
2019-11-25 02:02:55 -0300

    Work in navbar scroll transition - Desktop views

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,4 @@
-(function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     //Mobile button menu animation
     var el = document.getElementById("buttonMobile");
@@ -16,10 +16,58 @@
         elem.addEventListener('click', function () {
             el.classList.remove("c-button-mobile--active");
             menu.classList.remove("mobile-navigation__menu--active");
-        })
+        });
     })
+
+    //Navbar desktop transition
+    var posPrincipal = window.pageYOffset;
+
+    window.onscroll = function () {
+        var posActual = window.pageYOffset;
+
+        if (posPrincipal >= posActual) {
+            document.getElementById("navDesktop").classList.add("header-desktop");
+            document.getElementById("navDesktop").classList.remove("header-desktop--scrolling");
+
+        } else {
+            document.getElementById("navDesktop").classList.add("header-desktop--scrolling");
+        }
+
+        //Navbar desktop item list transition
+        var posVertical = window.scrollY;
+        var itemLista = document.querySelectorAll(".header-desktop ul li");
+        var item = Array.from(itemLista);
+
+        if (posVertical < 1420) {
+            item[0].classList.add("header-desktop--select");
+            item[1].classList.remove("header-desktop--select");
+            item[2].classList.remove("header-desktop--select");
+            item[3].classList.remove("header-desktop--select");
+
+        } else if (posVertical > 1430 && posVertical < 2100) {
+
+            item[0].classList.remove("header-desktop--select");
+            item[1].classList.add("header-desktop--select");
+            item[2].classList.remove("header-desktop--select");
+            item[3].classList.remove("header-desktop--select");
+
+        } else if (posVertical > 2100 && posVertical < 3700) {
+
+            item[0].classList.remove("header-desktop--select");
+            item[1].classList.remove("header-desktop--select");
+            item[2].classList.add("header-desktop--select");
+            item[3].classList.remove("header-desktop--select");
+
+        } else if (posVertical > 3700 && posVertical < 6000) {
+
+            item[0].classList.remove("header-desktop--select");
+            item[1].classList.remove("header-desktop--select");
+            item[2].classList.remove("header-desktop--select");
+            item[3].classList.add("header-desktop--select");
+        }
+    }
 
     //Smooth Scroll
     smoothie();
 
-})();
+});
